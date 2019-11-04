84664428ee982d7516875ab2baef055b359063ce
Галиуллин Ильдар <ldrgllln@gmail.com>
2019-11-04 16:16:48 +0400

    delete import

--- src/main/java/Vectorizer.java
+++ src/main/java/Vectorizer.java
@@ -1,14 +1,10 @@
 import java.io.*;
-import java.rmi.MarshalledObject;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Vectorizer {
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-        List<String> fileNames = Action.parseDirectory("C:\\Users\\User\\IdeaProjects\\xmlParsing\\txt_files");
+        List<String> fileNames = Action.parseDirectory("C:\\Users\\User\\IdeaProjects\\vectorizer\\txt_files");
         Action.createVectors(fileNames, "not_verse");
         long stop = System.currentTimeMillis() - start;
         System.out.println("Spend time: " + stop);
